9ecbc89481a4f448fb959ce3cd4537e8669f4567
Ramon Durães <ramonduraes@outlook.com>
2024-01-08 18:14:57 -0300

    Update

--- extensions/openai/basic/src/Adapters/Extensions/IntelligenceService/IntelligenceService.cs
+++ extensions/openai/basic/src/Adapters/Extensions/IntelligenceService/IntelligenceService.cs
@@ -2,112 +2,113 @@ using System.Collections.Generic;
 using Azure;
 using Azure.AI.OpenAI;
 
-namespace DevPrime.Extensions.IntelligenceService;
-public class IntelligenceService : DevPrimeExtensions, IIntelligenceService
+namespace DevPrime.Extensions.IntelligenceService
 {
-    private string Credential { get; set; }
-    private string URL { get; set; }
-    private string DeploymentModel { get; set; }
-
-
-    public IntelligenceService(IDpExtensions dp) : base(dp)
+    // Main class implementing the IIntelligenceService interface
+    public class IntelligenceService : DevPrimeExtensions, IIntelligenceService
     {
-        Credential = Dp.Settings.Default("ai.credential");
-        URL = Dp.Settings.Default("ai.url");
-        DeploymentModel = Dp.Settings.Default("ai.deploymentmodel");
-    }
+        // Private properties to store credentials and intelligence service configurations
+        private string Credential { get; set; }
+        private string URL { get; set; }
+        private string DeploymentModel { get; set; }
 
-    public string Conversation(string prompt)
-    {
-
-
-        return Dp.Pipeline(ExecuteResult: () =>
+        // Constructor initializing properties based on DpExtensions settings
+        public IntelligenceService(IDpExtensions dp) : base(dp)
         {
+            Credential = Dp.Settings.Default("ai.credential");
+            URL = Dp.Settings.Default("ai.url");
+            DeploymentModel = Dp.Settings.Default("ai.deploymentmodel");
+        }
 
-            Dp.Observability.Log("Starting OpenAI");
-
-            // Prepare AI Configurations
-            var chatCompletionsOptions = new ChatCompletionsOptions()
+        // Method to start a conversation with the AI service
+        public string Conversation(string prompt)
+        {
+            // Using DpExtensions pipeline to execute conversation logic
+            return Dp.Pipeline(ExecuteResult: () =>
             {
-                Messages =      {
-                                },
-                Temperature = (float)0.7,
-                MaxTokens = 800,
-                NucleusSamplingFactor = (float)0.95,
-                FrequencyPenalty = 0,
-                PresencePenalty = 0,
-                DeploymentName = DeploymentModel
-            };
+                // Log to indicate the start of interaction with OpenAI service
+                Dp.Observability.Log("Starting OpenAI");
 
+                // Settings for interacting with OpenAI service
+                var chatCompletionsOptions = new ChatCompletionsOptions()
+                {
+                    
+                    Temperature = (float)0.7,
+                    MaxTokens = 800,
+                    NucleusSamplingFactor = (float)0.95,
+                    FrequencyPenalty = 0,
+                    PresencePenalty = 0,
+                    DeploymentName = DeploymentModel
+                };
 
-            // Creating OpenAI Client
-            var openAiClient = new OpenAIClient(new System.Uri(URL), new AzureKeyCredential(Credential));
+                // Creating OpenAI client with provided credentials
+                var openAiClient = new OpenAIClient(new System.Uri(URL), new AzureKeyCredential(Credential));
 
-            // Add prompt to conversation 
-            //ChatRequestSystemMessage / ChatRequestUserMessage / ChatRequestAssistantMessage     
+                // Adding user's message to the conversation
+                // ChatRequestSystemMessage / ChatRequestUserMessage / ChatRequestAssistantMessage  
+                chatCompletionsOptions.Messages.Add(new ChatRequestUserMessage(prompt));
 
-            chatCompletionsOptions.Messages.Add(new ChatRequestUserMessage(prompt));
-           
+                // Preparation for interaction with OpenAI service
+                Response<ChatCompletions> response = openAiClient.GetChatCompletions(chatCompletionsOptions);
 
-            // Prepare for interaction
-            Response<ChatCompletions> response = openAiClient.GetChatCompletions (chatCompletionsOptions);
+                // Processing OpenAI response
+                var airesponse = response.GetRawResponse().Content.ToString();
+                var root = System.Text.Json.JsonSerializer.Deserialize<Root>(airesponse);
+                Message message = root.choices[0].message;
+                var airesult = message.content;
 
-            //OpenAI Result
-            var airesponse = response.GetRawResponse().Content.ToString();
-            var root = System.Text.Json.JsonSerializer.Deserialize<Root>(airesponse);
-            Message message = root.choices[0].message;
-            var airesult = message.content;
+                // Log to indicate the end of interaction with OpenAI service
+                Dp.Observability.Log("Finalizing OpenAI");
 
-            Dp.Observability.Log("Finalizing OpenAI");
-            return airesult;
-        });
+                // Returning OpenAI response
+                return airesult;
+            });
+        }
 
+        // Definition of inner classes to structure OpenAI response
+        public class ContentFilterResults
+        {
+            public bool filtered { get; set; }
+            public string severity { get; set; }
+        }
+
+        public class PromptFilterResults
+        {
+            public int prompt_index { get; set; }
+            public ContentFilterResults content_filter_results { get; set; }
+        }
+
+        public class Message
+        {
+            public string role { get; set; }
+            public string content { get; set; }
+            public ContentFilterResults content_filter_results { get; set; }
+        }
+
+        public class Choice
+        {
+            public int index { get; set; }
+            public string finish_reason { get; set; }
+            public Message message { get; set; }
+            public ContentFilterResults content_filter_results { get; set; }
+        }
+
+        public class Usage
+        {
+            public int prompt_tokens { get; set; }
+            public int completion_tokens { get; set; }
+            public int total_tokens { get; set; }
+        }
+
+        public class Root
+        {
+            public string id { get; set; }
+            public string _object { get; set; }
+            public int created { get; set; }
+            public string model { get; set; }
+            public List<PromptFilterResults> prompt_filter_results { get; set; }
+            public List<Choice> choices { get; set; }
+            public Usage usage { get; set; }
+        }
     }
-
-
-    public class ContentFilterResults
-{
-    public bool filtered { get; set; }
-    public string severity { get; set; }
-}
-
-public class PromptFilterResults
-{
-    public int prompt_index { get; set; }
-    public ContentFilterResults content_filter_results { get; set; }
-}
-
-public class Message
-{
-    public string role { get; set; }
-    public string content { get; set; }
-    public ContentFilterResults content_filter_results { get; set; }
-}
-
-public class Choice
-{
-    public int index { get; set; }
-    public string finish_reason { get; set; }
-    public Message message { get; set; }
-    public ContentFilterResults content_filter_results { get; set; }
-}
-
-public class Usage
-{
-    public int prompt_tokens { get; set; }
-    public int completion_tokens { get; set; }
-    public int total_tokens { get; set; }
-}
-
-public class Root
-{
-    public string id { get; set; }
-    public string _object { get; set; }
-    public int created { get; set; }
-    public string model { get; set; }
-    public List<PromptFilterResults> prompt_filter_results { get; set; }
-    public List<Choice> choices { get; set; }
-    public Usage usage { get; set; }
-}
-
 }
